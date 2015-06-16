92be904c5fc94c03baa70b1a6c460d71e577e827
programmers-index-investment <programmers.index.investment@gmail.com>
2015-06-16 20:21:55 +0900

    in June 2015

--- scripts/invest_return_graph.r
+++ scripts/invest_return_graph.r
@@ -5,7 +5,7 @@ t <- read.table("./invest_return.tsv", sep="\t", header=FALSE)
 x <- as.Date(t$V1, "%m/%d/%Y")
 y1 <- as.numeric(gsub(",","", t$V2))/1000000
 y2 <- as.numeric(gsub(",","", t$V3))/1000000
-png("../source/img/investment_return/investment_return.png")
+png("../images/investment_return/investment_return.png")
 y_range <- range(y1, y2)
 
 par(mar = c(3, 7, 2, 2))
@@ -14,7 +14,7 @@ par(mgp = c(3.5, 1, 0))
 plot(x, y1, xlab="", ylab="金額[百万円]", type="o", 
      cex.lab=2, cex.axis=1.5, cex.main=1.5, cex=2, ylim=y_range,
      xaxt="n",
-     yaxp = c(11, 16, 5),
+     yaxp = c(10, 20, 5),
      pch=1
      )
 lines(x, y2, xlab="", type="o", pch=16,
@@ -22,9 +22,9 @@ lines(x, y2, xlab="", type="o", pch=16,
      cex.lab=2, cex.axis=1.5, cex.main=1.5, cex=2, ylim=y_range,
      xaxt="n",
 # 1,100万円から1,600万円までを5分割で
-     yaxp = c(11, 16, 5)
+     yaxp = c(10, 20, 5)
      )
-x_years = as.Date(c("2014/8/1", "2015/1/1"), "%Y/%m/%d")
+x_years = as.Date(c("2014/8/1", "2015/1/1", "2015/6/1"), "%Y/%m/%d")
 axis(1, x_years, format(x_years, "%Y-%m"), cex=2, cex.axis=1.5, cex.main=1.5, cex=2, cex.lab=2)
 
 legend(x_years[1], y_range[2], c("評価額", "投資額"), cex=1.5, pch=c(16, 1))
